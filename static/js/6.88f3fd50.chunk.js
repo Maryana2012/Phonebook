ab46e4da5013470eaf81e923a940ddfa9070aa21
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2023-06-27 17:28:28 +0000

    Deploying to gh-pages from @ Maryana2012/goit-react-hw-08-phonebook@c142268084d629a7f10f31d4cbccb7f9cc1ed325 🚀

--- static/js/6.88f3fd50.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[6],{6:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var r=n(329);function a(){return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsx)("h1",{children:"Hello!!!"}),(0,r.jsx)("span",{children:" text of main page  "})]})}}}]);
-//# sourceMappingURL=6.88f3fd50.chunk.js.map
