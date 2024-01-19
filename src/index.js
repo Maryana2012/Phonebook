111fac1c53533c0e6c021a004440b242c3331782
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-01-19 10:44:02 +0200

    update name

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <ChakraProvider>
        <BrowserRouter
-        basename="/goit-react-hw-08-phonebook"
+        basename="/Phonebook"
        >
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
